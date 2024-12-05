aac5e5c4d93056fed02f637ec329d595c266d92a
mohitjangra117@gmail.com <mohitjangra117@gmail.com>
2024-12-05 18:18:59 +0530

    Added files

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+let h1=document.getElementById("iframe");
+let h2=h1.contentDocument;
+let h3=h2.getElementById("h1");
+
+
+function abc(){
+    let h1=document.getElementById("iframe");
+let h2=h1.contentDocument;
+let h3=h2.getElementById("h1");
+    if(h3){
+    h3.innerHTML="Changed";
+    }
+   
+
+
+}
+
